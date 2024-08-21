9e97342d5238c176fab896a1bbdd74ff77c71882
McKenna  Poulos <poulos.m@northeastern.edu>
2024-08-21 13:38:58 -0700

    add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,14 +7,14 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
       fontFamily: {
         sans: ["Neue-Haas-Grotesk-Text-Pro", "sans-serif"],
         serif: ["var(--font-juana)"],
+      },
+      colors: {
+        primary: "#b74e36",
+        secondary: "#c1c55e",
+        tertiary: "#eeebe7",
       },
     },
   },

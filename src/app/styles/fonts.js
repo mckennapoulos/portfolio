95ea450653aefd287d7f4c41a78a27141b8497e8
McKenna  Poulos <poulos.m@northeastern.edu>
2024-08-23 11:27:05 -0700

    re organize file structure - move assets to public

--- src/app/styles/fonts.js
+++ src/app/styles/fonts.js
@@ -3,12 +3,12 @@ import localFont from "next/font/local";
 export const JUANA = localFont({
   src: [
     {
-      path: "../assets/fonts/juana-regular-webfont.woff2",
+      path: "../../../public/fonts/juana-regular-webfont.woff2",
       weight: "400",
       style: "normal",
     },
     {
-      path: "../assets/fonts//juana-regularit-webfont.woff2",
+      path: "../../../public/fonts/juana-regularit-webfont.woff2",
       weight: "400",
       style: "italic",
     },

faa830b033c885ddd909cd2d44bca1e468ce49de
McKenna  Poulos <poulos.m@northeastern.edu>
2024-08-31 22:43:13 -0700

    update button"

--- src/_components/button/index.jsx
+++ src/_components/button/index.jsx
@@ -1,14 +1,24 @@
 import { twMerge } from "tailwind-merge";
 
 function Button(props) {
-  const { onClick, ariaLabel, text, classnames } = props;
+  const { onClick, label, children, classnames, disabled } = props;
+  const hoverStyles =
+    "hover:cursor-pointer hover:drop-shadow-button hover:translate-x-[3px] hover:translate-y-[-3px]";
+  const disabledStyles =
+    "aria-disabled:cursor-not-allowed aria-disabled:bg-secondary text-text border-none";
   const cx = twMerge(
-    "font-sm w-full rounded border border-primary bg-primary p-2 font-sans font-bold uppercase text-white hover:bg-tertiary hover:text-primary",
+    "font-sm w-full  md:w-auto rounded border border-primary bg-primary p-2 font-sans font-bold uppercase text-white  transition-all duration-500 ease-in-out",
+    disabled ? disabledStyles : hoverStyles,
     classnames,
   );
   return (
-    <button {...props} className={cx} onClick={onClick} aria-label={ariaLabel}>
-      {text}
+    <button
+      className={cx}
+      onClick={onClick}
+      aria-label={label}
+      aria-disabled={disabled}
+    >
+      {children}
     </button>
   );
 }

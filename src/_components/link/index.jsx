47fc929b762673c4b032156cdef253ccb178a706
McKenna  Poulos <poulos.m@northeastern.edu>
2024-09-04 20:57:13 -0700

    update link component to include inline and button styles

--- src/_components/link/index.jsx
+++ src/_components/link/index.jsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import PrimaryArrow from "../../../public/icons/arrow-primary.svg";
+import WhiteArrow from "../../../public/icons/arrow-white.svg";
 import ArrowSvg from "../../../public/icons/arrow.svg";
 import { twMerge } from "tailwind-merge";
 
@@ -29,18 +30,53 @@ export function Link({
   classnames,
   arrowClass,
   linkClass,
+  inline,
+  mobileButtonStyle,
 }) {
-  const cx = twMerge("group flex items-baseline", classnames);
+  const mobileButtonStyleCx =
+    "hover:drop-shadow-button rounded border border-primary bg-primary p-2 md:p-0 font-sans font-bold uppercase text-white hover:translate-x-[3px] hover:translate-y-[-3px] md:border-none md:bg-transparent md:normal-case md:hover:transform-none md:hover:drop-shadow-none transition ease-in-out duration-500";
+  const cx = twMerge(
+    "group flex items-baseline",
+    mobileButtonStyleCx,
+    classnames,
+  );
   const linkCx = twMerge(
-    "font-sans font-bold text-primary hover:cursor-pointer hover:underline",
+    "font-sans font-bold md:text-primary hover:cursor-pointer md:hover:underline",
     linkClass,
   );
+
+  if (inline) {
+    return (
+      <span className={cx}>
+        <a href={location} target="_blank">
+          <span className={linkCx}>{children}</span>
+        </a>
+        <Arrow variant="primary" classnames={arrowClass} />
+      </span>
+    );
+  }
+
   return (
     <div className={cx}>
-      <a href={location} target="_blank">
-        <span className={linkCx}>{children}</span>
-      </a>
-      <Arrow variant="primary" classnames={arrowClass} />
+      <div
+        className={mobileButtonStyle && "baseline m-auto flex md:m-0 md:block"}
+      >
+        <a href={location} target="_blank">
+          <span className={linkCx}>{children}</span>
+        </a>
+        {mobileButtonStyle && (
+          <Image
+            src={WhiteArrow}
+            alt="Northeast pointing arrow!"
+            width={12}
+            className="ml-1 block md:hidden"
+          />
+        )}
+      </div>
+      <Arrow
+        variant="primary"
+        classnames={twMerge(mobileButtonStyle && "hidden md:block", arrowClass)}
+      />
     </div>
   );
 }

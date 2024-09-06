44fce5cea373406c05bfdbb5379645160c6ae902
McKenna  Poulos <poulos.m@northeastern.edu>
2024-09-06 15:24:15 -0700

    update project schema

--- src/sanity/schemaTypes/project.js
+++ src/sanity/schemaTypes/project.js
@@ -1,5 +1,86 @@
 import { defineField, defineArrayMember } from "sanity";
-import { BiFile } from "react-icons/bi";
+import { BiFile, BiSolidMegaphone, BiHighlight } from "react-icons/bi";
+
+const customImage = {
+  name: "customImage",
+  title: "Image",
+  type: "image",
+  options: {
+    hotspot: true,
+  },
+  fields: [
+    {
+      title: "Alternative Text",
+      name: "alt",
+      type: "string",
+      validation: (Rule) => Rule.required(),
+      options: {
+        isHighlighted: true,
+      },
+    },
+    {
+      title: "Caption",
+      name: "caption",
+      type: "string",
+    },
+  ],
+};
+
+const BlockElement = {
+  type: "block",
+  styles: [
+    {
+      title: "Normal",
+      value: "normal",
+    },
+    {
+      title: "H5",
+      value: "h5",
+      component: ({ children }) => (
+        <span className="font-sans text-3xl tracking-tight">{children}</span>
+      ),
+    },
+    {
+      title: "H6",
+      value: "h6",
+      component: ({ children }) => (
+        <span className="font-sans text-3xl font-bold tracking-tight text-primary">
+          {children}
+        </span>
+      ),
+    },
+    {
+      title: "Quote",
+      value: "blockquote",
+      component: ({ children }) => (
+        <span className="font-sans text-5xl text-primary">"{children}"</span>
+      ),
+    },
+  ],
+  marks: {
+    decorators: [
+      { title: "Strong", value: "strong" },
+      { title: "Emphasis", value: "em" },
+      { title: "Underline", value: "underline" },
+      {
+        title: "Highlight",
+        value: "highlight",
+        icon: BiHighlight,
+        component: ({ children }) => (
+          <span className="text-primary">{children}</span>
+        ),
+      },
+      {
+        title: "Callout",
+        value: "callout",
+        icon: BiSolidMegaphone,
+        component: ({ children }) => (
+          <span className="bg-tertiary">{children}</span>
+        ),
+      },
+    ],
+  },
+};
 
 const project = {
   name: "project",
@@ -21,9 +102,54 @@ const project = {
       validation: (rule) => rule.required(),
     },
     {
+      name: "linkOnly",
+      title: "Only show as link",
+      type: "boolean",
+    },
+    {
       name: "href",
       title: "Project link",
       type: "url",
+    },
+    defineField({
+      name: "categories",
+      type: "array",
+      title: "Categories of project",
+      validation: (rule) => rule.required(),
+      of: [
+        defineArrayMember({
+          type: "object",
+          name: "category",
+          fields: [{ type: "string", name: "name" }],
+        }),
+      ],
+    }),
+    defineField({
+      name: "tech",
+      type: "array",
+      title: "Tech pills for a project",
+      of: [
+        defineArrayMember({
+          type: "object",
+          name: "pill",
+          fields: [{ type: "string", name: "name" }],
+        }),
+      ],
+    }),
+    {
+      name: "duration",
+      title: "Duration of project",
+      type: "string",
+    },
+
+    {
+      name: "team",
+      type: "object",
+      title: "Project team breakdown",
+      fields: [
+        { type: "string", name: "role" },
+        { type: "number", name: "memberCount" },
+      ],
     },
     {
       name: "previewImage",
@@ -52,55 +178,29 @@ const project = {
       type: "file",
     },
     defineField({
-      name: "categories",
-      type: "array",
-      title: "Categories of project",
-      validation: (rule) => rule.required(),
-      of: [
-        defineArrayMember({
-          type: "object",
-          name: "category",
-          fields: [{ type: "string", name: "name" }],
-        }),
-      ],
-    }),
-    {
-      name: "duration",
-      title: "Duration of project",
-      type: "string",
-    },
-
-    {
-      name: "team",
-      type: "object",
-      title: "Project team breakdown",
-      fields: [
-        { type: "string", name: "role" },
-        { type: "number", name: "memberCount" },
-      ],
-    },
-    defineField({
-      name: "tech",
-      type: "array",
-      title: "Tech pills for a project",
-      of: [
-        defineArrayMember({
-          type: "object",
-          name: "pill",
-          fields: [{ type: "string", name: "name" }],
-        }),
-      ],
-    }),
-    {
       name: "overview",
-      type: "object",
-      title: "High-level overview of the project",
-      fields: [
-        { type: "text", name: "client" },
-        { type: "text", name: "problem" },
-        { type: "text", name: "impact" },
+      type: "array",
+      title: "High-level overview section",
+      of: [
+        defineArrayMember({
+          type: "object",
+          name: "section",
+          fields: [
+            { type: "string", name: "title" },
+            {
+              name: "content",
+              title: "Section content",
+              type: "array",
+              of: [
+                {
+                  type: "block",
+                },
+              ],
+            },
+          ],
+        }),
       ],
-    },
+    }),
     {
       name: "process",
       title: "Full process of project",
@@ -111,34 +211,44 @@ const project = {
           name: "discovery",
           title: "Discovery process block",
           type: "array",
-          of: [
-            { type: "block" },
-            {
-              type: "image",
-            },
-          ],
+          of: [BlockElement, customImage],
         },
         {
           name: "desgin",
           title: "Design process block",
           type: "array",
-          of: [
-            { type: "block" },
-            {
-              type: "image",
-            },
-          ],
+          of: [BlockElement, customImage],
         },
         {
           name: "development",
           title: "Development process block",
           type: "array",
-          of: [
-            { type: "block" },
-            {
-              type: "image",
-            },
-          ],
+          of: [BlockElement, customImage],
+        },
+      ],
+    },
+    {
+      name: "screens",
+      title: "Project screens",
+      type: "object",
+      fields: [
+        {
+          name: "desktop",
+          title: "Desktop screens",
+          type: "array",
+          of: [customImage],
+        },
+        {
+          name: "mobile",
+          title: "Mobile screens",
+          type: "array",
+          of: [customImage],
+        },
+        {
+          name: "tablet",
+          title: "Tablet screens",
+          type: "array",
+          of: [customImage],
         },
       ],
     },

3bd5c8caa49e894da6f9ab0480dfe50cdd30c045
McKenna  Poulos <poulos.m@northeastern.edu>
2024-09-09 11:37:19 -0700

    add basic boundary pages

---
+++ src/app/error.js
@@ -0,0 +1,11 @@
+"use client";
+
+import { Heading } from "@/_components/text";
+
+export default function Error() {
+  return (
+    <main className="mx-auto flex min-h-screen w-full max-w-screen-2xl flex-col px-5 pb-5 pt-14">
+      <Heading type="h3">Error &#9785; &#x2639;</Heading>
+    </main>
+  );
+}
---
+++ src/app/loading.js
@@ -0,0 +1,9 @@
+import { Heading } from "@/_components/text";
+
+export default function Loading() {
+  return (
+    <main className="mx-auto flex min-h-screen w-full max-w-screen-2xl flex-col px-5 pb-5 pt-14">
+      <Heading type="h3">Loading...</Heading>
+    </main>
+  );
+}

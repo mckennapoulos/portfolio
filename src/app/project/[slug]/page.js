03351df9b3ad758079263167a2d5d919ee94b458
McKenna  Poulos <poulos.m@northeastern.edu>
2024-09-09 15:40:16 -0700

    add next and prev project data

--- src/app/project/[slug]/page.js
+++ src/app/project/[slug]/page.js
@@ -14,14 +14,54 @@ export async function generateStaticParams() {
   });
 }
 
+function CreateNav(projectList, current) {
+  let projectIndex = {
+    current: { key: 0, slug: current },
+    prev: { key: 0, slug: null },
+    next: { key: 0, slug: null },
+  };
+
+  for (let i = 0; i < projectList.length; i++) {
+    if (projectList[i].title === current) {
+      projectIndex.current.key = i;
+      if (i === 0) {
+        projectIndex.prev.key = projectList.length - 1;
+        projectIndex.next.key = i + 1;
+        projectIndex.prev.slug = projectList[projectIndex.prev.key].slug;
+        projectIndex.next.slug = projectList[projectIndex.next.key].slug;
+        break;
+      } else if (i === projectList.length - 1) {
+        projectIndex.next.key = 0;
+        projectIndex.prev.key = i - 1;
+        projectIndex.prev.slug = projectList[projectIndex.prev.key].slug;
+        projectIndex.next.slug = projectList[projectIndex.next.key].slug;
+        break;
+      } else {
+        projectIndex.next.key = i + 1;
+        projectIndex.prev.key = i - 1;
+        projectIndex.prev.slug = projectList[projectIndex.prev.key].slug;
+        projectIndex.next.slug = projectList[projectIndex.next.key].slug;
+        break;
+      }
+    }
+  }
+
+  return projectIndex;
+}
+
 export default async function Project({ params }) {
   const { slug } = params;
   const data = await client.fetch(
     `*[_type == "project"&&lower(@.slug)==lower("${slug}")][0]`,
   );
+  const projectList = await client.fetch(
+    `*[_type == "project" && slug != null] | order(_createdAt asc)`,
+  );
+  const index = CreateNav(projectList, data.title);
+
   return (
-    <div className="mx-auto flex min-h-screen w-full max-w-screen-2xl flex-col p-5">
-      <CaseStudy data={data} />
+    <div className="mx-auto flex min-h-screen w-full max-w-screen-2xl flex-col p-5 pt-14">
+      <CaseStudy data={data} internalNav={index} />
     </div>
   );
 }

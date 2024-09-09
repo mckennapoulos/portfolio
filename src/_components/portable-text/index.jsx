3b2fc9d0ed472e56b17aa4d89046e956b0c1371f
McKenna  Poulos <poulos.m@northeastern.edu>
2024-09-09 11:43:38 -0700

    style portable text

--- src/_components/portable-text/index.jsx
+++ src/_components/portable-text/index.jsx
@@ -2,22 +2,30 @@ import { PortableText } from "@portabletext/react";
 import { Heading, Paragraph } from "../text";
 import { twMerge } from "tailwind-merge";
 import { Link } from "../link";
+import { SanityImage } from "../image";
 
 const myPortableTextComponents = {
   types: {
+    customImage: ({ value }) => {
+      return <SanityImage {...value} alt={value.alt} classnames="my-12" />;
+    },
     image: ({ value }) => <img src={value.imageUrl} />,
-    callToAction: ({ value, isInline }) =>
-      isInline ? (
-        <a href={value.url}>{value.text}</a>
-      ) : (
-        <div className="callToAction">{value.text}</div>
-      ),
+    callout: ({ value }) => {
+      return (
+        <div className="w-full bg-tertiary px-12 py-16">
+          <Paragraph classnames="font-sans text-3xl tracking-tight text-primary font-bold normal-case mb-4">
+            {value.heading}
+          </Paragraph>
+          <PortableTextComponent blocks={value.content} />
+        </div>
+      );
+    },
   },
   block: {
     h5: ({ children }) => (
       <Heading
         type="h5"
-        classnames="normal-case text-xl text-text font-bold mb-1"
+        classnames="font-sans text-3xl tracking-tight text-text normal-case mt-12 mb-2"
       >
         {children}
       </Heading>
@@ -27,17 +35,34 @@ const myPortableTextComponents = {
         {children}
       </Heading>
     ),
+    blockquote: ({ children }) => (
+      <p className="my-8 font-sans text-5xl font-bold leading-[60px] tracking-tight text-primary">
+        "{children}"
+      </p>
+    ),
   },
   marks: {
     // Ex. 2: rendering a custom `link` annotation
     link: ({ value, children }) => {
       // account for internal links
       return (
-        <Link location={value?.href} inline={true}>
+        <Link location={value?.href} inline={true} classnames="mt-2">
           {children}
         </Link>
       );
     },
+  },
+  list: {
+    bullet: ({ children }) => <ul className="my-4 list-disc">{children}</ul>,
+    number: ({ children }) => <ol className="my-4 list-decimal">{children}</ol>,
+  },
+  listItem: {
+    // Ex. 1: customizing common list types
+    bullet: ({ children }) => <li className="ml-4">{children}</li>,
+    number: ({ children }) => <li className="ml-5">{children}</li>,
+
+    // // Ex. 2: rendering custom list items
+    // checkmarks: ({children}) => <li>✅ {children}</li>,
   },
 };
 

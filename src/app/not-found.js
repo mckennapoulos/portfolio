16a9b26ea1ba4afef26018d9039ea3de7e092886
McKenna  Poulos <poulos.m@northeastern.edu>
2024-10-03 15:58:10 -0700

    text change

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -3,7 +3,7 @@ import { Heading, Paragraph } from "@/_components/text";
 export default function NotFound() {
   return (
     <div className="mx-auto flex min-h-screen w-full max-w-screen-2xl flex-col px-5 pb-5 pt-14">
-      <Heading type="h3">Not found</Heading>
+      <Heading type="h3">Not found &#9785;</Heading>
       <Paragraph>
         I can't find what you're looking for. Check the url and please try
         again.
